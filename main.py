d756ef9fff85c9507f0db3ba532987b1bce1a306
albinjerkers <albinjerkers@gmail.com>
2025-12-03 19:41:53 +0100

    Added dictator 3000 and updated report output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, dict_sorter
 
 def get_book_text(path):
     with open(path, encoding="utf-8") as f:
@@ -9,5 +9,13 @@ def get_book_text(path):
 
 PATH = "books/frankenstein.txt"
 if __name__ == "__main__":
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {PATH}")
+    print("----------- Word Count ----------")
     print(f"Found {count_words(get_book_text(PATH))} total words")
-    print(count_chars(get_book_text(PATH)))
+    print("--------- Character Count -------")
+    for i in dict_sorter(count_chars(get_book_text(PATH))):
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
+
--- stats.py
+++ stats.py
@@ -8,3 +8,11 @@ def count_chars(text):
     for char in text.lower():
         chars[char] = chars.get(char, 0) + 1
     return chars
+
+def dict_sorter(char_dict):
+    dicts = []
+    for char, value in char_dict.items():
+        dicts.append({"char": char,"num":value})
+        dicts.sort(reverse=True,key=lambda x: x["num"])
+    return dicts
+
