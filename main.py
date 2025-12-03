b53b7607c6b3085ca450e804a98a41c2cbc858b4
albinjerkers <albinjerkers@gmail.com>
2025-12-03 19:49:01 +0100

    added sys variables and removed hard coded path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import count_words, count_chars, dict_sorter
-
+import sys
 def get_book_text(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
@@ -7,15 +7,20 @@ def get_book_text(path):
 
 
 
-PATH = "books/frankenstein.txt"
+
 if __name__ == "__main__":
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {PATH}")
-    print("----------- Word Count ----------")
-    print(f"Found {count_words(get_book_text(PATH))} total words")
-    print("--------- Character Count -------")
-    for i in dict_sorter(count_chars(get_book_text(PATH))):
-        if i["char"].isalpha():
-            print(f"{i["char"]}: {i["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        PATH = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {PATH}")
+        print("----------- Word Count ----------")
+        print(f"Found {count_words(get_book_text(PATH))} total words")
+        print("--------- Character Count -------")
+        for i in dict_sorter(count_chars(get_book_text(PATH))):
+            if i["char"].isalpha():
+                print(f"{i["char"]}: {i["num"]}")
+        print("============= END ===============")
 
